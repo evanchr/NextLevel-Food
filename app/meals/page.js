7ee216d770616c547b6dbc4ba331b593a3747115
Evan CHARRIER <evanc378549@gmail.com>
2024-12-03 19:06:40 +0100

    add SQLIte database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
-      <header className={styles.header}></header>
+      <header className={styles.header}>
       <h1>
         Des plats délicieux, créés <span className={styles.highlight}>par vous</span>
       </h1>
@@ -13,8 +16,9 @@ export default function MealsPage() {
       <p className={styles.cta}>
         <Link href="/meals/share">Partagez une recette</Link>
       </p>
+      </header>
       <main className={styles.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals}/>
       </main>
     </>
   );
