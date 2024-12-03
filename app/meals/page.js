8e1f9730d949a896cae6a36941fefb3748a43fb8
Evan CHARRIER <evanc378549@gmail.com>
2024-12-03 17:49:53 +0100

    add container for meals in Meals Page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-      <Link href="/meals/share">Partager</Link>
-      <Link href="/meals/1">Meals with slug</Link>
-    </main>
+    <>
+      <header className={styles.header}></header>
+      <h1>
+        Des plats délicieux, créés <span className={styles.highlight}>par vous</span>
+      </h1>
+      <p>Choisissez votre recette favorite et cuisinez la. C'est facile et amusant!</p>
+      <p className={styles.cta}>
+        <Link href="/meals/share">Partagez une recette</Link>
+      </p>
+      <main className={styles.main}>
+        <MealsGrid meals={[]}/>
+      </main>
+    </>
   );
 }
