976ba3ab2714246dd2160ba616028ae994c6f272
Evan CHARRIER <evanc378549@gmail.com>
2024-12-05 12:18:15 +0100

    add a not-found page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Cette pas page n'existe pas</h1>
+      <p>Malheureusement, nous ne trouvons pas la page ou la ressource demandée.</p>
+    </main>
+  );
+}
