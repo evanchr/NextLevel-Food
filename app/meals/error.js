f5b1c9c61939a791b6b1759a6846e6c9c9aeb6ce
Evan CHARRIER <evanc378549@gmail.com>
2024-12-05 12:17:52 +0100

    add an error page

---
+++ app/meals/error.js
@@ -0,0 +1,8 @@
+'use client';
+
+export default function Error() {
+    return <main className="error">
+        <h1>Une erreur s'est produite !</h1>
+        <p>Chargement des plats échoué. Merci de réessayer plus tard.</p>
+    </main>
+}
