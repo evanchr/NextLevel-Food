a93cd622c49493a18f541c7d208455afd87a7fb4
Evan CHARRIER <evanc378549@gmail.com>
2024-12-05 12:55:59 +0100

    add not found page for meal details

---
+++ app/meals/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <main className="not-found">
+        <h1>Ce plat n'existe pas</h1>
+        <p>Malheureusement, nous ne trouvons pas la page ou le plat demandé.</p>
+      </main>
+    );
+  }
+  
